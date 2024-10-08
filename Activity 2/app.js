980d78b4a2ec281452e782e6c1508959ad2f413a
Acenathaniel22 <sinagacenathaniel@gmail.com>
2024-10-08 17:47:48 +0800

    New

---
+++ Activity 2/app.js
@@ -0,0 +1,49 @@
+const title = document.getElementById('song-title');
+const artist = document.getElementById('artist-name');
+const addbutton = document.getElementById('add-button');
+const ul = document.getElementById('songlist');
+
+addbutton.addEventListener('click', ()=>{
+    const newTitle = title.value;
+    const newArtist = artist.value;
+
+    //create element  
+    const p = document.createElement('p');
+    const small = document.createElement('small');
+    const div = document.createElement('div');
+    const li = document.createElement('li');
+    const button = document.createElement('button');
+    li.append(p);
+    li.append(small);
+    li.append(button);
+    
+    //append to list
+    ul.append(li);
+
+
+
+    console.log(li);
+
+
+    
+ 
+    
+
+
+    //set value to the element
+    p.innerHTML = newTitle;
+    small.innerHTML = newArtist;
+    button.innerHTML = "DELETE" 
+
+    //add class to element
+    p.classList.add('song-title');
+    small.classList.add('artist-name');
+
+
+    //create container
+
+
+
+})
+
+console.log(title, artist, addbutton)
